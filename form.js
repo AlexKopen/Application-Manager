b395f24834077a91ec5801fc6b12ef324d02a025
akopen <alexkopen@gmail.com>
2015-08-17 15:22:09 -0500

    JSON data generation and output

---
+++ form.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	//Serialize all form data and convert to JSON on submit
+
+	var jsonData;
+	var spanFields;
+
+	$('#simple-form').submit(function(event) {
+		jsonData = JSON.stringify($(this).serializeArray());
+		spanFields = $('#simple-form span');
+		event.preventDefault();
+	});
+
+
+	//Display JSON as a form
+	
+	$('#display-button').click(function(event) {
+		var jsonAsArray = $.parseJSON(jsonData);
+		for (var i = 0; i < jsonAsArray.length; i++) {
+			$('#form-display form').append('<span>' + spanFields.eq(i).text() + '</span><br>' + '<input type="text" name = "' + jsonAsArray[i].name + '"' + 'value = "' + jsonAsArray[i].value + '"><br>');
+		};		
+	});
+});
