6532e81be2ee60708183c5270e457174fd331824
Alex Kopen <alexkopen@gmail.com>
2015-09-07 17:47:26 -0500

    Starting to finalize things on public and admin end

--- admin/display_application.php
+++ admin/display_application.php
@@ -3,27 +3,41 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Reiser Relief Application</title>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="fancy-style.css">
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<title>Application Manager</title>
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="app_style.css">
 </head>
 <body>
 
-<?php require ('fetch_application.php'); ?>
+<div id="header">
+	<div class = "container">
+		<div class="pure-g">
+			<div class="pure-u-1-2" id = "left-header">
+				<div id = "logo-container">
+					<img src="logo.png">
+				</div>
+			</div>
 
-<div id="form-display">
-	<div class="container">
-		<?php require('form_template.php'); ?>
+			<div class="pure-u-1-2" id = "right-header">
+			<?php  
+				if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+					echo '<p><a href = "logout.php">Logout</a></p>';
+				}
+			?>	
+			</div>
+		</div>
 	</div>
 </div>
+
+<?php require ('fetch_application.php'); ?>
+
+<?php 
+	require ('form_template.php');
+ ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="app.display.js"></script>
