b65c381e5f03b4f29fc3ff911c60a53af337d19d
James Vorderbruggen <jamesvorder@gmail.com>
2015-09-07 17:30:34 -0500

    Added a table heading
    Added a thead element as well as tbody for semantics.  Added content to the thead.

--- public/fetch_all_dates.php
+++ public/fetch_all_dates.php
@@ -9,7 +9,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
-    echo '<table class = "dates">';
+    echo '<table class = "dates pure-table pure-table-bordered"><thead><tr><th>Trip Date</th><th></th></tr></thead><tbody>';
     $oddRow = true;
 
     // output data of each row
@@ -32,13 +32,13 @@ if ($result->num_rows > 0) {
         echo $tripDateFormatted;
         echo '</td>';
         echo '<td>';
-        echo ('<div class = "trip"><p><a href = "application.php?trip=' . $row['date'] . '">Apply</a></p></div>');
+        echo ('<div class = "trip"><p><a class="apply-link" href = "application.php?trip=' . $row['date'] . '">Apply</a></p></div>');
         echo '</td>';              
         echo '</tr>';
 
     }
 
-    echo '</table>';
+    echo '</tbody></table>';
 
 } else {
     echo '<p>No Dates Found</p>';
