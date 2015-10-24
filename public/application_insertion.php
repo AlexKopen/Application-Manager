40a9f28b91640c0555cebdd55bd610d5a0d99509
akopen <alexkopen@gmail.com>
2015-10-23 23:01:18 -0500

    Email functionality

--- public/application_insertion.php
+++ public/application_insertion.php
@@ -21,6 +21,11 @@ $json = $_GET['json'];
 $sql = "INSERT INTO applications (name, date, trip, json) VALUES ('$name', '$date', '$trip', '$json')";
 
 if ($conn->query($sql) === TRUE) {
+
+	// Send an email notifying of the recent application
+	require ('email.php');
+	sendMail($trip);
+
     echo "success";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
---
+++ public/email.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function sendMail($tripDate){
+
+	$recipients = array('alexkopen@gmail.com');
+	$subject = 'Haiti Mission Trip Application';
+
+    $tripDate = new DateTime($tripDate);
+    $tripDateFormatted = $tripDate->format('l\, F jS\, Y');
+
+	$message = 'An application has just been submitted for the trip of ' . $tripDateFormatted . '.  Please access the admin portal to view the application.';
+
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
+	$headers .= 'From: <hosting@alexkopen.com>' . "\r\n";
+
+	for ($i = 0; $i < count($recipients); $i++) { 
+		mail($recipients[$i], $subject, $message, $headers);
+	}
+
+}
+
+?>
