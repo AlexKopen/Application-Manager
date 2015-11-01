b90f840993dba28e983ddeb33ac32833ae4c86f2
Alex Kopen <alexkopen@gmail.com>
2015-11-01 15:56:20 -0600

    Email message spaces

--- public/email.php
+++ public/email.php
@@ -9,7 +9,7 @@ function sendMail($tripDate){
     $tripDate = new DateTime($tripDate);
     $tripDateFormatted = $tripDate->format('l\, F jS\, Y');
 
-	$message = 'An application was submitted for the trip of ' . $tripDateFormatted . '.  <a href = "reiserrelief.org/apply/admin">Please access the administrator portal to view the application.</a>Also, Alex Kopen spent a lot of time programming this, so please consider buying the poor college student a drink next time you see him.';
+	$message = 'An application was submitted for the trip of ' . $tripDateFormatted . '.  <a href = "reiserrelief.org/apply/admin">Please access the administrator portal to view the application.</a>  Also, Alex Kopen spent a lot of time programming this, so please consider buying the poor college student a drink next time you see him.';
 
 	// Headers to prevent the email being flagged as spam, to allow for HTML in messages, and specifying the return email address
 	$headers = 'MIME-Version: 1.0' . "\r\n";
