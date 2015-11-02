70e49cf273708cf3ce3d664cb68278f18da2cd91
akopen <alexkopen@gmail.com>
2015-11-01 20:14:53 -0600

    First attempts to change login because of Hostgator

--- admin/login.php
+++ admin/login.php
@@ -1,70 +1,52 @@
 <?php 
 
-	// DB credentials and session start
-	require("connect.php");
+// DB credentials and session start
+require("connect.php");
 
-    if(!empty($_POST)) { 
+if(!empty($_POST)) {
 
-		$username = $_POST['username'];
-		$login_ok = false;
+	$query = 'SELECT * FROM users WHERE username = ?';
+	$stmt = $conn->prepare($query);
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	$stmt->bind_result($Username);
 
-		if($stmt = $conn -> prepare("SELECT * FROM users WHERE username = ?")) {
+	while ($stmt->fetch()){
+		// // Encryption check
+		// $check_password = hash('sha256', $_POST['password'] . $row['salt']);		
 
-			// Statement functionality
-			$stmt -> bind_param("s", $username);
-			$stmt -> execute();
-			$result = $stmt->get_result();
-			$stmt -> close();
+		// for($round = 0; $round < 65536; $round++) { 
+		// 	$check_password = hash('sha256', $check_password . $row['salt']); 
+		// } 
+		 
+		// if($check_password === $row['password']) {
+		// 	$login_ok = true; 
+		// }
 
-			// See if user exists
-			if ($result->num_rows > 0) {
-
-				while ($row = $result->fetch_assoc()) {
-					// Encryption check
-					$check_password = hash('sha256', $_POST['password'] . $row['salt']);		
-
-					for($round = 0; $round < 65536; $round++) { 
-						$check_password = hash('sha256', $check_password . $row['salt']); 
-					} 
-					 
-					if($check_password === $row['password']) {
-						$login_ok = true; 
-					}
-
-					// Login success
-					if($login_ok) { 
-						// Remove sensitive data from $row for best practice
-						unset($row['salt']); 
-						unset($row['password']); 
-						 
-						// Session user data
-						$_SESSION['user'] = $row; 
-						 
-						// Redirect the user to desired page 
-						header("Location: home.php"); 
-						die("Redirecting to: home.php"); 
-					} else { 
-						// Invalid password
-						header("Location: index.php"); 
-						die("Redirecting to: index.php"); 
-					}
-				}
-
-			} else {
-				// User does not exist
-				header("Location: index.php"); 
-				die("Redirecting to: index.php"); 	
-			}
-
-		} else{
-			// Invalid query
-			header("Location: index.php"); 
-			die("Redirecting to: index.php"); 		    
-		}
-
-	} else{
-		// Form was not submitted
-		header("Location: index.php"); 
-		die("Redirecting to: index.php"); 	
+		// // Login success
+		// if($login_ok) { 
+		// 	// Remove sensitive data from $row for best practice
+		// 	unset($row['salt']); 
+		// 	unset($row['password']); 
+			 
+		// 	// Session user data
+		// 	$_SESSION['user'] = $row; 
+			 
+		// 	// Redirect the user to desired page 
+		// 	header("Location: home.php"); 
+		// 	die("Redirecting to: home.php"); 
+		// } else {
+		// 	// Invalid password
+		// 	header("Location: index.php"); 
+		// 	die("Redirecting to: index.php"); 
+		// }   
+		echo $Username;
 	}
-?> 
+
+} else {
+	// Form was not submitted
+	header("Location: index.php"); 
+	die("Redirecting to: index.php"); 	
+}
+
+?>
